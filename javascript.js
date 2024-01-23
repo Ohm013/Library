a07835d1804eebe41125dd5096a9ebd386ea4cce
Ohm Patel <60417541+Ohm013@users.noreply.github.com>
2024-01-23 11:55:05 -0600

    able to print the user input onto the screen. It is not understanding the read checkbox. Need a way to create a new prototype everytime add book is clicked

--- javascript.js
+++ javascript.js
@@ -1,51 +1,33 @@
-//First confused on how to start in general, how would the data typed trasnfer to js and then turn into an object that is pushed into an array
-//Solution: first query selected the form elements and made sure to add ".value" at the end so now we have the user inputs value
-//.value will get what the user has written
 
-// Problem 2: How to make user inputs into one object 
-
-
-//How to display stuff that is in the array, would need to loop over it for sure.
-//
-
-
-//If there is only one button in a form, the browser will default use it as a way to send the user inputs somewhere
-//So to stop the form from submitting you use ".preventDefault();"
 const addBookBtn = document.querySelector('.addBook');
-const title = document.querySelector('#book_name').value ;
-const author = document.querySelector('#author').value;
-const pages = document.querySelector('#pages').value;
-const read = document.querySelector('#read').value; //make sure to add value or youd have to everytime you mention the variable
 const myBooks = document.querySelector('.myBooks');
-
 
 const myLibrary = [];
 
-function Book () {
-  
+addBookBtn.addEventListener('click', addBookToLibrary); //once you click add book, it should send results to myLibrary Array
 
+
+function Book (title,author,pages,read) {
+  this.title = title;
+  this.author = author ;
+  this.pages = pages;
+  this.read = read;
 }
 
 
 
-
-addBookBtn.addEventListener('click', addBookToLibrary); //once you click add book it should send results to myLibrary Array
-
 function addBookToLibrary(e) {
+  const title = document.querySelector('#book_name').value ;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const read = document.querySelector('#read'); //make sure to add value or youd have to everytime you mention the variable
 
-    e.preventDefault();
+  e.preventDefault();
 
-    let book = {
-        title: title ,
-        author: this.author,
-        pages: this.pages ,
-        read: this.read 
+  const input = new Book (title,author,pages,read);
 
-        
-    }
-    console.log(title);
-    myLibrary.push(book);
-    myBooks.textContent = myLibrary;
+  //need to make it where each submit creates a prototype of the book
+  
 
 }
 
@@ -57,3 +39,32 @@ function addBookToLibrary(e) {
 //}
 
 //showBooks() ;
+
+//Problem 1: confused on how to start in general, how would the data typed trasnfer to js and then turn into an object that is pushed into an array
+//Solution 1: first query selected the form elements and made sure to add ".value" at the end so now we have the user inputs value
+//.value will get what the user has written
+
+//Problem 2: How to make user inputs into one object 
+//Solution 2: add event listener to add me button to initiate addBookToLibrary function to make user inputs into one object.
+
+
+//How to display stuff that is in the array, would need to loop over it for sure.
+//
+
+
+//If there is only one button in a form, the browser will default use it as a way to send the user inputs somewhere
+//So to stop the form from submitting to somewhere you use ".preventDefault();"
+
+//Problem 4: It is not properly reading correctly if the book has been read or not when box is checked
+
+let book = {
+  title ,
+  author,
+  pages ,
+  read
+  //not recognizing the book title, authors etc.  Recognized after I put the querySelected elements inside the object
+}
+
+myLibrary.push(" Title: " + book.title + " Author: " + book.author + " Pages: " + book.pages + " Read? " + book.read);
+myBooks.textContent = myLibrary; 
+console.log(myLibrary);
