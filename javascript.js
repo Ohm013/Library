121bf625ac87d6d4d2c73cfbdf2209e7288f9385
Ohm Patel <60417541+Ohm013@users.noreply.github.com>
2024-01-24 13:17:36 -0600

    textContent is able to display the array of objects properties, but now it wont create a seperate card for each book, it adds all the objects into one card

--- javascript.js
+++ javascript.js
@@ -4,8 +4,7 @@ const myBooks = document.querySelector('.myBooks');
 
 const myLibrary = [];
 
-addBookBtn.addEventListener('click', addBookToLibrary); //once you click add book, it should send results to myLibrary Array
-
+//myLibrary.forEach(input => myBooks.textContent = Object.values(input));
 
 function Book (title,author,pages,read) {
   this.title = title;
@@ -15,6 +14,7 @@ function Book (title,author,pages,read) {
 }
 
 
+addBookBtn.addEventListener('click', addBookToLibrary); //once you click add book, it should send results to myLibrary Array
 
 function addBookToLibrary(e) {
   const title = document.querySelector('#book_name').value ;
@@ -24,21 +24,25 @@ function addBookToLibrary(e) {
 
   e.preventDefault();
 
-  const input = new Book (title,author,pages,read);
+  const input = new Book (title,author,pages,read); //need to make it where each submit creates a prototype of the book
 
-  //need to make it where each submit creates a prototype of the book
-  
+  myLibrary.push(input);
+  displayBooks();
+}
 
+function displayBooks (){
+    for (let i = 0; i < myLibrary.length ; i++) {
+
+      const card = document.createElement("div"); //trying to create a new card everytime a book is submitted
+      card.textContent = " Title: " + myLibrary[i].title + " Author: " + myLibrary[i].author + " Pages: " + myLibrary[i].pages + " Read? " + myLibrary[i].read; 
+      myBooks.appendChild(card);
+    } //got the display to work. Doesn't show one book at a time, will show all of them now.
+      // the "myLirary[i].title, etc is how u can pinpoint each object value"
 }
 
 
-//function showBooks (){
-  //  for (let i = 0; i >= myLibrary.length - 1 ; i++) {
-    //    console.log([i]);
-   // }
-//}
 
-//showBooks() ;
+
 
 //Problem 1: confused on how to start in general, how would the data typed trasnfer to js and then turn into an object that is pushed into an array
 //Solution 1: first query selected the form elements and made sure to add ".value" at the end so now we have the user inputs value
@@ -48,8 +52,8 @@ function addBookToLibrary(e) {
 //Solution 2: add event listener to add me button to initiate addBookToLibrary function to make user inputs into one object.
 
 
-//How to display stuff that is in the array, would need to loop over it for sure.
-//
+//Problem 3: How to display stuff that is in the array, would need to loop over it for sure. I tried myBooks.textContent = myLibrary but it displays [object Object] not the actual object properties just showing it is an object in the array.
+// Solution 4: 
 
 
 //If there is only one button in a form, the browser will default use it as a way to send the user inputs somewhere
@@ -57,14 +61,14 @@ function addBookToLibrary(e) {
 
 //Problem 4: It is not properly reading correctly if the book has been read or not when box is checked
 
-let book = {
-  title ,
-  author,
-  pages ,
-  read
+//let book = {
+  //title ,
+  //author,
+  //pages ,
+  //read
   //not recognizing the book title, authors etc.  Recognized after I put the querySelected elements inside the object
-}
+//}
 
-myLibrary.push(" Title: " + book.title + " Author: " + book.author + " Pages: " + book.pages + " Read? " + book.read);
-myBooks.textContent = myLibrary; 
-console.log(myLibrary);
+//myLibrary.push(" Title: " + book.title + " Author: " + book.author + " Pages: " + book.pages + " Read? " + book.read);
+//myBooks.textContent = myLibrary; 
+//console.log(myLibrary);
